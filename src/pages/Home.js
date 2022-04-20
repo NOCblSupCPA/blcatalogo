7c02a4231a8eb337d854f630c63070e38cad3d30
NOCblSupCPA <nocblcpa@gmail.com>
2022-04-20 17:18:14 -0300

    1.0.1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Image, ImageBackground} from 'react-native';
 import { ScrollView,TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
@@ -8,98 +8,113 @@ import Bloco from '../components/Bloco';
 import { SimpleLineIcons } from '@expo/vector-icons'; 
 import AppIntroSlider from 'react-native-app-intro-slider';
 
+const slides = [
+    {
+        key:'1',
+        image: require('../assets/360MB1024.png')
+    },
+    {
+        key:'2',
+        image:require('../assets/bl02.png')
+    },
+    {
+        key:'3',
+        image:require('../assets/grupobl.png')
+    }
+]
+
 export default function Home() {
 
+    function render({item}){
+        return (
+            <View style={{flex:1}}>
+                <Image
+                source={item.image}
+                style={{ resizeMode:'cover',
+                         width: '100%',
+                         height: '100%',
+                    }}
+                />
+                
+            </View>
+          );
+    }
+    const [showHome, setShowHome] = useState(false);
     const navigation = useNavigation();
-    
-    return (
-        <View style={{flex:1, backgroundColor:'#f5f5f5'}}>
-            <View style={styles.container}>
-                <View style={styles.services}>
-                    <Text style={{fontFamily:'Montserrat_400Regular',
-                    fontSize: 19,
-                    paddingTop:5,
-                    marginLeft:10,
-                    }}>Serviços</Text>
-                </View>
-                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                <View style={{flexDirection:'row'}}>
-                    <Bloco name= "LIKE FIBRA"
-                    description="• Instalação Gratuita" 
-                    description1="• Wi-Fi Ultra"
-                    description2="• APP - Paramount +"
-                    description3 ="• APP - Noggin"
-                    onPress={() => 
-                    navigation.navigate('Internet')
-                    }
-                    />
-                    <Bloco name= "LIKE TV" 
-                    description="• LIGHT TV"
-                    description1="• LIKE BASIC"
-                    description2="• LIKE PLUS"
-                    description3="• LIKE TOTAL"
-                    onPress={() =>
-                    navigation.navigate('Tv')
-                    }/>
-                    <Bloco name= "COMBO" 
-                    description="•LIGHT"
-                    description1="•LIFE"
-                    description2="•ULTRA"
-                    onPress={() => 
-                    navigation.navigate('Combo')
-                    }/>
-                    <Bloco name= "NOVIDADES EM BREVE..." 
-                    description="" 
-                    onPress={() => 
-                    navigation.navigate('News')
-                    }/>
-                </View>
-            </ScrollView>
-            </View>
-
-            <View style={styles.subtitle}>
-                <Text style={styles.text}>Para sua casa</Text>
-            </View> 
-                <View style={{flex:2}}>
-                    <View style={{alignItems:'center', }}>
-                        <View style={styles.header}>
-                            <Image source={require('../assets/360MB1024.png')} style={{resizeMode:'contain',
-                            width:370,
-                            height:342,
-                            borderRadius:9,
-                            }}/>
-                        </View>
+    if(showHome){
+        return <Text>Entrou na home</Text>
+    }
+    else{
+        return (
+            <View style={{flex:1, backgroundColor:'#f5f5f5'}}>
+                <View style={styles.container}>
+                    <View style={styles.services}>
+                        <Text style={{fontFamily:'Montserrat_400Regular',
+                        fontSize: 19,
+                        paddingTop:5,
+                        marginLeft:10,
+                        }}>Serviços</Text>
                     </View>
-                    <View style={{flex:1}}>             
-                        <View style={{paddingTop:10}}>
-                                <Text style={{fontFamily:'Montserrat_400Regular',
-                                fontSize:15,
-                                marginLeft:10}}>NOSSOS APPS</Text>
-                        </View>
-                        <View style={styles.imgs}>
-                                <Image source={require('../assets/nogginbl.png')} style={{resizeMode:'contain',
-                            width:140,
-                            height:60,
-                            marginHorizontal:15}}/>
-
-                                <Image source={require('../assets/paramount1.png')} style={{resizeMode:'contain',
-                            width:140,
-                            height:60,
-                            marginHorizontal:15}}/>
-                        </View>
+                    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                    <View style={{flexDirection:'row'}}>
+                        <Bloco name= "LIKE FIBRA"
+                        description="• Instalação Gratuita" 
+                        description1="• Wi-Fi Ultra"
+                        description2="• APP - Paramount +"
+                        description3 ="• APP - Noggin"
+                        onPress={() => 
+                        navigation.navigate('Internet')
+                        }
+                        />
+                        <Bloco name= "LIKE TV" 
+                        description="• LIGHT TV"
+                        description1="• LIKE BASIC"
+                        description2="• LIKE PLUS"
+                        description3="• LIKE TOTAL"
+                        onPress={() =>
+                        navigation.navigate('Tv')
+                        }/>
+                        <Bloco name= "COMBO" 
+                        description="•LIGHT"
+                        description1="•LIFE"
+                        description2="•ULTRA"
+                        onPress={() => 
+                        navigation.navigate('Combo')
+                        }/>
+                        <Bloco name= "NOVIDADES EM BREVE..." 
+                        description="" 
+                        onPress={() => 
+                        navigation.navigate('News')
+                        }/>
                     </View>
+                </ScrollView>
+                </View>
+
+                <View style={{marginTop:10, width:'100%', height:'55%'}}>  
+                <Text style={{fontFamily:'Montserrat_400Regular', marginBottom:5, fontSize:20}}>PARA SUA CASA</Text>         
+                <AppIntroSlider
+                renderItem={render}
+                data={slides}
+                activeDotStyle={{
+                    backgroundColor:'#fff',
+                    width:20,
+                }}
+                showDoneButton={false}
+                showNextButton={false}
+                />
+                </View>
             </View>
-        </View>
-    );
-}
+        );
+    }
+    }
+
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        //flex:1,
         backgroundColor:'#62AE47',
-        //height:'35%',
+        height:'35%',
     },
     subtitle:{ 
-
     },
     text:{
         marginLeft:10,
